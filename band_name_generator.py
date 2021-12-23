8e52da2ba0c000d7879fe480c9e0431ab3d45d9a
Mark Shortridge <mshortridge@gmail.com>
2021-12-23 13:41:56 -0600

    Finished Band Name Generator File.

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,3 +1,7 @@
 # Mark Shortridge
 # 12-23-2021
 #
+print("Welcome to the Band Name Geberator.")
+city = input("What's the name of the city you grew up in?")
+pet = input("What is the name of one of your pets?")
+print(f"Your new band name is: {city} {pet}!")
