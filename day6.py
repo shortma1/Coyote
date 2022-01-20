b9ad4bed427c49da15b90d4ccc6a579f10ac4610
Mark Shortridge <20524903+shortma1@users.noreply.github.com>
2022-01-20 17:05:48 -0600

    Create day6.py
    Added day6.py

---
+++ day6.py
@@ -0,0 +1,27 @@
+# # functions
+# print("Hello")  # print() is a function
+# num_char = len("Hello")  # len() is also a function
+# print(num_char)
+
+
+# def my_function():  # def defines function, my_function() is the name of the function, and : finished the function definition
+#     print("Hello")
+#     print("Bye")
+
+# my_function()  # to call the function, otherwise it will not run
+
+# Defining Functions
+# def my_function():
+#  #Do this
+#  #Then do this
+# Then Call the function
+# my_function()
+
+# While Loops
+# while something is True
+# do something
+
+number = 6
+while number > 0:
+    print(f"Number is: {number}")
+    number -= 1
